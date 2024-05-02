c036e66207190d6b94713c034be60976c7bc9cae
Grishamdarooka <163041883+Grishamdarooka@users.noreply.github.com>
2024-05-02 23:28:06 +0530

    Update area_volume.m

--- area_volume.m
+++ area_volume.m
@@ -1,7 +1,7 @@
 clc 
 clear
 
-fprintf('This is a code for getiing the area and volume of a cuboide \n\n')
+fprintf('This is a code for getiing the area and volume of a cuboide \n')
 fprintf('Please provide with the model values \n\n')
 
 l = input('Enter the length of the cuboid in cm =  ');
@@ -11,6 +11,6 @@ h = input('Enter the height of the cuboid in cm =  ');
 a = 2*(l*h + h*b + b*l);
 v = l*b*h;
 
-fprintf('\n\n Area of the cuboid is %10.2f cm2',a)
-fprintf('\n Volume of the cuboid is %10.2f cm3',v)
+fprintf('\nArea of the cuboid is %10.2f cm2',a)
+fprintf('\nVolume of the cuboid is %10.2f cm3',v)
 
